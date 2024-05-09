8e880d6310aefec1d711c9ce0349b544a5d31809
Prathmesh3080 <prathmeshpatil843@gmail.com>
2024-05-09 12:33:28 +0530

    Add New Project
    create and push the project on Git

---
+++ taskOne/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserTableComponent } from './user-table/user-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
