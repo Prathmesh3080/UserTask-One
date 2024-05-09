b2348ca67a36f2dce0c109e119dee44ff409b35e
Prathmesh3080 <prathmeshpatil843@gmail.com>
2024-05-09 13:16:54 +0530

    Add
    Add API URL and Array of Object

--- taskOne/src/app/app.module.ts
+++ taskOne/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserTableComponent } from './user-table/user-table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { UserTableComponent } from './user-table/user-table.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
--- taskOne/src/app/user-table/user-table.component.ts
+++ taskOne/src/app/user-table/user-table.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-user-table',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class UserTableComponent {
 
+  userList:any []=[];
+  headArray = [
+    {'Head': 'User Id', 'FieldName': 'id'},
+    {'Head': 'User Name', 'FieldName': 'name'},
+    {'Head': 'Email', 'FieldName': 'email'},
+    {'Head': 'Website', 'FieldName': 'website'},
+    {'Head': 'Action', 'FieldName': ''}
+  ];
+
+  /**
+   *
+   */
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void{
+    this.loadUser();
+  }
+
+  loadUser(){
+
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((result:any) =>{
+      this.userList = result;
+    });
+  }
+
+  editUser(item:any){
+    debugger;
+  }
+
+   deleteUser(item:any){
+    debugger;
+   } 
 }
